6b9cdd825bb0d762de9ec5fe0317b5b2d999419e
Nicolas De Cook <nicolasdecook@gmail.com>
2026-07-25 19:45:22 +0200

    f1 tracker v3

--- api/_lib/lollipopmanLookup.d.ts
+++
@@ -1,5 +0,0 @@
-export interface LollipopmanMatch {
-    videoId: string | null;
-    title?: string;
-}
-export declare function findLollipopmanVideo(apiKey: string, raceDate: string, keywords: string[]): Promise<LollipopmanMatch>;
